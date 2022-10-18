61bc7d526b3542037c058c9c854734ed2b327179
LinaM20 <c19366191@mytudublin.ie>
2022-10-18 22:33:09 +0100

    amended problem 2

--- Lab4/problem-2/script.mjs
+++ Lab4/problem-2/script.mjs
@@ -1,30 +1,18 @@
 import fetch from "node-fetch";
 
-// fetch('https://jsonplaceholder.typicode.com/posts')
-// .then((response) => {
-//   return response.json();
-// })
-// .then((data) => {
-//   let arrayData = [];
-//   arrayData.push(data);
-//   console.log(arrayData);
-
-//   let obj = arrayData.find(o => {
-//     return o.title.length > 6;
-//   });
-//   console.log(obj);
-
-// })
-
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json())
 .then(posts => console.log(posts.filter(posts => titleCount(posts.title))))
 
+console.log(posts.filter(posts => frequency(posts.body)))
+
 function titleCount(title) {
-  let posts = [];
-  posts.push(title);
-  console.log(posts);
+  const posts = title.split(' ');
 
   console.log('RESULT:');
-  return posts.filter(title => title.length > 6);
+  return posts.filter(word => word !== '').length > 6;
+}
+
+function frequency(body) {
+  
 }

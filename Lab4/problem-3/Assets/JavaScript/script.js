d2712a38c70ec5cfb5ecac3f56cbbaa55f198bf2
LinaM20 <c19366191@mytudublin.ie>
2022-10-24 16:00:30 +0100

    Fixed the repos div

--- Lab4/problem-3/Assets/JavaScript/script.js
+++ Lab4/problem-3/Assets/JavaScript/script.js
@@ -7,24 +7,45 @@ document.getElementById("search-button").addEventListener("click", function(){
         return res.json()})
 
         .then(function(data) { 
-
+            console.log(data)
             document.querySelector("#image").src = data.avatar_url;
             document.querySelector("#username").innerText = data.login;
             document.querySelector("#fullname").innerText = data.name;
             document.querySelector("#location").innerText = data.location;
-            document.querySelector("#email").innerText = data.emails;
+            document.querySelector("#email").innerText = data.email;
             document.querySelector("#number-of-gists").innerText = data.public_gists;
 
-            
-            document.querySelector("#repo-name").innerHTML = data.repos; 
-            console.log(data.repos);
+            document.querySelector("#repo-name").innerHTML = data.repos_url; 
             document.querySelector("#repo-description").innerHTML = data.repos.description;
-            
-            
-
-
+      
         }) 
         .catch(function(error) { 
-                document.querySelector("title").innerText=error;
+                document.querySelector("title").innerText = error;
+        })
+
+        fetch(`https://api.github.com/users/${username.value}/repos`)
+        .then(function(res) {
+        return res.json()})
+        .then(function(data){
+            console.log(data);
+            
+            let html = "";
+
+            for (var i = 0; i < data.length; i++) {
+                html += `
+                        <div id="user-repos">
+                            <div id="repo-name">${data[i].name}</div>
+                            <div id="repo-description">${data[i].description}</div>
+                        </div>`
+        
+                         
+            }
+
+            
+
+            let repoDiv = document.querySelector("#user-repos-container"); 
+            repoDiv.innerHTML = html;
+           
         })
     });
+    

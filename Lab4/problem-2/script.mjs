80f1ad26dd5107acc95eab1f171775529f817ebb
LinaM20 <c19366191@mytudublin.ie>
2022-10-31 10:16:29 +0000

    Attempted problem 2 lab 4 again

--- Lab4/problem-2/script.mjs
+++ Lab4/problem-2/script.mjs
@@ -3,7 +3,11 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 .then(posts => {
   console.log("Titles more than 6")
   let wordResult = posts.filter(posts => titleCount(posts.title))
-  console.log(wordResult);
+  console.log(wordResult)
+
+  console.log("Frequency Map")
+  let frequencyCount = posts.map(posts => frequencyMap(posts.body))
+  console.log(frequencyCount)
 
 });
 
@@ -11,4 +15,23 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 function titleCount(title) {
   const posts = title.split(' ');
   return posts.filter(word => word !== '').length > 6;
+}
+
+//Frequency map to show the frequency of words
+function frequencyMap(body) {
+  let results = {};
+  let replaceWords = body.replaceAll('\n', ' ');
+  let words = replaceWords.split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    
+    if(results[word] === undefined) {
+      results[word] = 1;
+    } else {
+      results[word] += 1;
+    }
+  }
+
+  return results;
 }

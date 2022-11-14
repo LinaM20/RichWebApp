b433cc803e96f93101d11973f834562a1a477da2
LinaM20 <c19366191@mytudublin.ie>
2022-11-14 19:30:44 +0000

    Click buttons with console display

--- Lab8/problem-2/index.js
+++ Lab8/problem-2/index.js
@@ -1,5 +1,21 @@
 const { fromEvent } = rxjs;
 
-var button = document.getElementById('start-button');
+let button = document.getElementById('start-button');
+let hourInput = document.getElementById('hours');
+let minutesInput = document.getElementById('minutes');
+let secondsInput = document.getElementById('seconds');
+const form = html
+    '<div><p>HEllo</p></div>';
 const click = fromEvent(button, 'click')
-click.subscribe(() => console.log('click'));
+click.subscribe(() => {
+    console.log("Hour: " + hourInput.value);
+    console.log("Minutes: " + minutesInput.value);
+    console.log("Seconds: " + secondsInput.value);
+    form;
+}); 
+
+
+
+
+
+

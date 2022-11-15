b182e2a9f034a7dd937ac5378bb113a315f2ea29
LinaM20 <c19366191@mytudublin.ie>
2022-11-15 19:01:49 +0000

    work in progress

--- Lab8/problem-2/Assets/JavaScript/index.js
+++ Lab8/problem-2/Assets/JavaScript/index.js
@@ -1,29 +1,66 @@
 const { fromEvent } = rxjs;
 
 let button = document.getElementById('start-button');
+
 let hourInput = document.getElementById('hours');
 let minutesInput = document.getElementById('minutes');
 let secondsInput = document.getElementById('seconds');
-let mySeconds = hourInput.value;
+
+let countHours = 0;
+let countMinutes = 0;
+let countSeconds = 0;
+let startCount = 0;
 
 rxjs.fromEvent(button, 'click')
-.subscribe(() => showTimer());
+.subscribe(() => startTimer());
 
-function tick() {
+function startTimer() {
+    if(startCount == 0 && document.getElementById('hours') && document.getElementById('minutes') && document.getElementById('seconds')) {
+        countHours = parseInt(document.getElementById('hours').value) + 0;
+        countMinutes = parseInt(document.getElementById('minutes').value) + 0;
+        countSeconds = parseInt(document.getElementById('seconds').value) * 1;
+        if(isNaN(countHours)) countHours = 0;
+        if(isNaN(countMinutes)) countMinutes = 0;
+        if(isNaN(countSeconds)) countSeconds = 0;
+        
+        document.getElementById('hours').value = countHours;
+        document.getElementById('minutes').value = countMinutes;
+        document.getElementById('seconds').value = countSeconds;
+            
+        startCount = 1;
+        document.getElementById('start-button').setAttribute('disabled', 'disabled');
     
+    }
+    if(countHours == 0 && countMinutes == 0 && countSeconds == 0) {
+        startCount = 0;
+        document.getElementById('start-button').removeAttribute('disabled');
+        
+        return false;
+      }
+      else {
+        countSeconds--;
+        if(countSeconds < 0) {
+          if(countMinutes > 0) {
+            if(countHours > 0) {
+                countSeconds = 59;
+                countMinutes--;
+                countHours--;
+            }
+            
+          }
+          else {
+            countSeconds = 0;
+            countMinutes = 0;
+            countHours = 0;
+          }
+        }
+      }
+
+
+    document.getElementById('displayHours').innerHTML = countHours;
+    document.getElementById('displayMinutes').innerHTML = countMinutes;
+    document.getElementById('displaySeconds').innerHTML = countSeconds;
+
+    setTimeout('startTimer()', 1000);
+
 }
-
-function showTimer() {
-    console.log("hours" + hourInput.value);
-    let div = document.createElement('div');
-    div.innerHTML = "the hour is " + hourInput.value;
-    document.body.appendChild(div);
-
-    let intervalHandler = setInterval(tick, 1000);
-    
-}
-
-
-
-
-

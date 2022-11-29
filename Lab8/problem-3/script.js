eecdda4687ababa379e5af21b3a261dc9e7f8a71
LinaM20 <c19366191@mytudublin.ie>
2022-11-29 22:06:43 +0000

    fixe delete child note functions

--- Lab8/problem-3/script.js
+++ Lab8/problem-3/script.js
@@ -1,6 +1,7 @@
 const { fromEvent } = rxjs;
 
 showNotes();
+showChildNotes();
 
 //Click listener when user adds a note
 
@@ -8,7 +9,16 @@ let addBtn = document.getElementById("add-note-button");
 rxjs.fromEvent(addBtn, 'click')
     .subscribe(() => addNote()
 );
-//let addBtn = document.getElementById("add-note-button");
+
+let deleteBtn = document.getElementById("delete-note-button");
+rxjs.fromEvent(deleteBtn, 'click')
+    .subscribe(() => deleteNote(index)
+);
+
+let editBtn = document.getElementById("edit-note-button");
+rxjs.fromEvent(editBtn, 'click')
+    .subscribe(() => editNote(index)
+);
 
 function addNote() {
 
@@ -34,6 +44,7 @@ function addNote() {
     message.value = "";
   
     showNotes();
+    showChildNotes();
 }
 
 //Show the notes in the notes section
@@ -55,12 +66,14 @@ function showNotes() {
 		<p class="message"> ${element.text}</p>
 		<button id="${index}"onclick="deleteNote(this.id)" class="delete">Delete Note</button>
 		<button id="${index}"onclick="editNote(this.id)" class="edit">Edit Note</button>
-        <button id="${index}"onclick="addNote(this.id)" class="add">Add Note</button>
+        <button id="${index}"onclick="addChildNote(this.id)" class="add">Add Child Note</button>
 	</ul>`;
     });
   
     let notesElement = document.getElementById("notes");
     notesElement.innerHTML = html;
+    console.log(notesArray);
+
 
 }
 
@@ -68,18 +81,25 @@ function showNotes() {
 function deleteNote(index) {
 
     let notes = localStorage.getItem("notes");
+    let childNotes = localStorage.getItem("child-notes");
   
     if (notes == null) {
         notesArray = [];
+        childNotesArray = [];
       } else {
         notesArray = JSON.parse(notes);
+        childNotesArray = JSON.parse(childNotes);
     }
   
     notesArray.splice(index, 1);
+    childNotesArray.splice(index, 1);
   
     localStorage.setItem("notes", JSON.stringify(notesArray));
+    localStorage.setItem("child-notes", JSON.stringify(childNotesArray));
+
   
     showNotes();
+    showChildNotes();
 }
 
 //Editing the notes
@@ -98,9 +118,82 @@ function editNote(index) {
     message.value = notesArray[index].text;
     colour.value = notesArray[index].colours;
 
-    notesArray.remove(index, 1);
+    notesArray.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesArray));
 
     showNotes();
+    showChildNotes();
 	
 }
+
+function addChildNote(index) {
+    let message = document.getElementById("message");
+    let colour = document.getElementById("color");
+
+    let childNotes = localStorage.getItem("child-notes");
+
+    if (childNotes == null) {
+        childNotesArray = [];
+      } else {
+        childNotesArray = JSON.parse(childNotes);
+    }
+
+    let notesMessage = {
+        text: message.value,
+        colours: colour.value
+    };
+  
+    childNotesArray.push(notesMessage);
+    localStorage.setItem("child-notes", JSON.stringify(childNotesArray));
+
+    message.value = "";
+  
+    showChildNotes();
+
+}
+
+function showChildNotes() {
+
+    let childNotes = localStorage.getItem("child-notes");
+
+    if (childNotes == null) {
+        childNotesArray = [];
+      } else {
+        childNotesArray = JSON.parse(childNotes);
+    }
+  
+    let html = "";
+  
+    childNotesArray.forEach(function(element, index) {
+        html += ` <ul id="child-note" style="background-color: ${element.colours}">
+		<p class="note-counter">Child note ${index + 1}</p>
+		<p class="message"> ${element.text}</p>
+		<button id="${index}"onclick="deleteChildNote(this.id)" class="delete">Delete Child Note</button>
+		<button id="${index}"onclick="editNote(this.id)" class="edit">Edit Note</button>
+        <button id="${index}"onclick="addChildNote(this.id)" class="add">Add Child Note</button>
+	</ul>`;
+    });
+  
+    let notesElement = document.getElementById("child-notes");
+    notesElement.innerHTML = html;
+    console.log("child");
+    console.log(childNotesArray);
+}
+
+// function deleteChildNote(index) {
+
+//     let childNotes = localStorage.getItem("child-notes");
+  
+//     if (notes == null) {
+//         childNotesArray = [];
+//       } else {
+//         childNotesArray = JSON.parse(childNotes);
+//     }
+  
+//     childNotesArray.splice(index, 1);
+  
+//     localStorage.setItem("child-notes", JSON.stringify(childNotesArray));
+  
+//     showNotes();
+//     showChildNotes();
+// }

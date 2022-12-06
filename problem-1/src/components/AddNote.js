05367e6f4f79e656e8a9731c78a9ff606cb6a9ab
LinaM20 <c19366191@mytudublin.ie>
2022-12-06 23:27:01 +0000

    addded files and some addnote code

---
+++ problem-1/src/components/AddNote.js
@@ -0,0 +1,23 @@
+const AddNote = () => {
+    return (
+        <div className="note-text">
+                <h2 id="note-editor-title">Take Notes!</h2>
+                <textarea onChange={handleChange} value={noteText} className="message" name="message" placeholder="Notes, notes, notes..."></textarea>
+                <label>Colours</label>
+                <select className="color" name="color">
+                    <option value="#FFABAB">Red</option>
+                    <option value="#F29f44">Orange</option>
+                    <option value="#FFF5BA">Yellow</option>
+                    <option value="#E7FFAC">Green</option>
+                    <option value="#ACE7FF">Blue</option>
+                    <option value="#D5AAFF">Purple</option>
+                </select>
+                <div id="add-button">
+                    <button className="add-note-button" onClick={handleSaveClick}>Add Note</button>
+                </div>
+            </div>
+    );
+};
+
+export default AddNote;
+

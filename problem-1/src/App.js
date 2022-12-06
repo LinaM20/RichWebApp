52d254dd6041c73dc9e4e228d53af7b36ff1b69e
LinaM20 <c19366191@mytudublin.ie>
2022-12-06 23:50:59 +0000

    added styling from previous notes app and note app code

--- problem-1/src/App.js
+++ problem-1/src/App.js
@@ -1,5 +1,37 @@
+import NotesList from "./components/NotesList";
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+
 const App = () => {
-  return <p>hello</p>;
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my note',
+      //color: color
+    }
+  ]);
+
+  const addNote = (text) => {
+    const newNote = {
+      id: nanoid(),
+      text: text,   
+      //color: color,
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  return (<div className="display-notes">
+    <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote={deleteNote}/>
+  </div>
+  );
+
+  
 };
 
 export default App;
